b05e55260890083fb66f5846a4385fbcc31e85cd
Alex Stevanoski <alexanderleigh89@gmail.com>
2023-10-06 10:06:11 +1100

    added code

--- API endpoint testing project/Program.cs
+++ API endpoint testing project/Program.cs
@@ -1,6 +1,27 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using System;
+using System.Net.Http;
+using RestSharp;
+using RestSharp.Authenticators;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
 
-app.MapGet("/", () => "Hello World!");
+namespace ChallengesApp
+{
+    public class ChuckNorris
+    {
+        public static void ChuckNorriss()
+        {
+            string url = "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random";
+            
+            var client = new RestClient(url);
+            var request = new RestRequest();
+            request.AddHeader("X-RapidAPI-Key", "95420226abmshc17434483ca7701p1afebajsn23899967f720");
+            request.AddHeader("X-RapidAPI-Host", "matchilling-chuck-norris-jokes-v1.p.rapidapi.com");
 
-app.Run();
+            var response = client.Get(request);
+
+            Console.WriteLine(response.Content);
+            Console.Read();
+        }
+    }
+}
