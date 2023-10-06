4011ea85d5a6310fb22fd57a6c3e7fb12f3455de
Alex Stevanoski <alexanderleigh89@gmail.com>
2023-10-06 23:23:46 +1100

    Commit from VS Community

--- API endpoint testing project/Program.cs
+++ API endpoint testing project/Program.cs
@@ -5,23 +5,22 @@ using RestSharp.Authenticators;
 using System.Threading.Tasks;
 using System.Net.Http.Headers;
 
-namespace APITesting
+namespace APITesting;
+
+public class MainClass
 {
-    public class MainClass
+    public static void Main()
     {
-        public static void Main()
-        {
-            string url = "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random";
-            
-            var client = new RestClient(url);
-            var request = new RestRequest();
-            request.AddHeader("X-RapidAPI-Key", "95420226abmshc17434483ca7701p1afebajsn23899967f720");
-            request.AddHeader("X-RapidAPI-Host", "matchilling-chuck-norris-jokes-v1.p.rapidapi.com");
+        string url = "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random";
+        
+        var client = new RestClient(url);
+        var request = new RestRequest();
+        request.AddHeader("X-RapidAPI-Key", "95420226abmshc17434483ca7701p1afebajsn23899967f720");
+        request.AddHeader("X-RapidAPI-Host", "matchilling-chuck-norris-jokes-v1.p.rapidapi.com");
 
-            var response = client.Get(request);
+        var response = client.Get(request);
 
-            Console.WriteLine(response.Content);
-            Console.Read();
-        }
+        Console.WriteLine(response.Content);
+        Console.Read();
     }
 }
